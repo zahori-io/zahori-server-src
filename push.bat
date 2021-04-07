33614b19fba83c17477ecdba0c1b64ed731cc559
Jose Turegano Barbero <jose.turegano@panel.es>
2021-04-07 14:07:53 +0200

    Spring-boot 2.4.4 update. Added properties for sonarcloud.io Added profile to build containers with jib in the future. Dependency update to avoid security vulnerabilities.

--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 set DOCKER_BUILDKIT=1
-docker tag zahori-server:0.1.0 zahoriaut/zahori-server:0.1.0
-docker push zahoriaut/zahori-server:0.1.0
+docker tag zahori-server:0.1.2 zahoriaut/zahori-server:0.1.2
+docker push zahoriaut/zahori-server:0.1.2
