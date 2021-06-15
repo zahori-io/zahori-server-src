4fb2fa1285bb7216137098239122e58d2702cfb4
Jose Turegano Barbero <jose.turegano@panel.es>
2021-06-15 11:52:27 +0200

    Update version

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set DOCKER_BUILDKIT=1
-docker tag zahoriaut/zahori-server:0.1.4-SNAPSHOT zahoriaut/zahori-server:0.1.4-SNAPSHOT
-docker push zahoriaut/zahori-server:0.1.4-SNAPSHOT
+docker tag zahoriaut/zahori-server:0.1.6 zahoriaut/zahori-server:0.1.6
+docker push zahoriaut/zahori-server:0.1.6
