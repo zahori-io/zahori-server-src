d19f4de124344b52cfe1247124bc3fed50da19e6
David Vazquez <davidvazqueznovella@gmail.com>
2023-02-24 09:48:33 +0100

    Update build scripts to version 0.1.16

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DOCKER_BUILDKIT=1 docker build -o backend/src/main/resources/static/ frontend
-DOCKER_BUILDKIT=1 docker build -t zahoriaut/zahori-server:0.1.15 backend
+DOCKER_BUILDKIT=1 docker build -t zahoriaut/zahori-server:0.1.16 backend
--- push.bat
+++ push.bat
@@ -1,1 +1,1 @@
-docker push zahoriaut/zahori-server:0.1.15
+docker push zahoriaut/zahori-server:0.1.16
