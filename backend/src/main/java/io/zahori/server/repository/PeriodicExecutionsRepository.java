8855fb1de839a324d1fe36ac475788ca1fd6536c
David Vazquez <davidvazqueznovella@gmail.com>
2023-06-19 13:04:18 +0200

    Add missing license headers

--- backend/src/main/java/io/zahori/server/repository/PeriodicExecutionsRepository.java
+++ backend/src/main/java/io/zahori/server/repository/PeriodicExecutionsRepository.java
@@ -1,5 +1,28 @@
 package io.zahori.server.repository;
 
+/*-
+ * #%L
+ * zahori-server
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2021 - 2023 PANEL SISTEMAS INFORMATICOS,S.L
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import io.zahori.server.model.PeriodicExecution;
 import io.zahori.server.model.PeriodicExecutionView;
 import java.util.List;
