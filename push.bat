5a66df33c017363fe7b488934e1bca174a4022b4
David Vazquez <davidvazqueznovella@gmail.com>
2023-06-19 12:53:01 +0200

    Update build scripts to version 0.1.17

--- push.bat
+++ push.bat
@@ -1,1 +1,1 @@
-docker push zahoriaut/zahori-server:0.1.16
+docker push zahoriaut/zahori-server:0.1.17
--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker push zahoriaut/zahori-server:0.1.16
+docker push zahoriaut/zahori-server:0.1.17
