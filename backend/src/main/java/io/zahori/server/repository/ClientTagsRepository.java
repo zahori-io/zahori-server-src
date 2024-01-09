11936303543af2854f5c3eae9aedea5791c61959
David Vazquez <davidvazqueznovella@gmail.com>
2024-01-09 12:08:21 +0100

    get tags ordered by name

--- backend/src/main/java/io/zahori/server/repository/ClientTagsRepository.java
+++ backend/src/main/java/io/zahori/server/repository/ClientTagsRepository.java
@@ -12,24 +12,22 @@ package io.zahori.server.repository;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
  */
-
 import io.zahori.server.model.ClientTag;
 import java.util.Set;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 
 /**
  * The interface ClientsEnvironments repository.
@@ -44,6 +42,6 @@ public interface ClientTagsRepository extends CrudRepository<ClientTag, Long> {
      * @param processId the process id
      * @return the set
      */
-    @Query("SELECT t FROM ClientTag t WHERE t.client.clientId = :clientId and t.process.processId = :processId and t.active = true ORDER BY t.order ASC")
+    @Query("SELECT t FROM ClientTag t WHERE t.client.clientId = :clientId and t.process.processId = :processId and t.active = true ORDER BY t.name ASC")
     Set<ClientTag> findByClientIdAndProcessId(@Param("clientId") Long clientId, @Param("processId") Long processId);
 }
